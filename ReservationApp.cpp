5ecc2955a13900750ddcf79f5b184881804e661e
goodOffice <senamabel@hotmail.com>
2021-11-30 17:00:38 -0500

    Added gitignore

---
+++ ReservationApp.cpp
@@ -0,0 +1,19 @@
+﻿// ReservationApp.cpp : Defines the entry point for the application.
+//
+
+#include "ReservationApp.h"
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+
+int main()
+{
+    using namespace std;
+    using namespace nana;
+
+
+
+
+    //Start to event loop process, it blocks until the form is closed.
+    exec();
+}
